bf118338da4cc201aa38f87e342ad50e8881a0c5
IvanFestina <ivanfestina@gmail.com>
2022-01-07 17:41:38 +0300

    doing HW 5

---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import HW6 from "../../h6/HW6";
+
+function Junior() {
+    return (
+        <div>
+            <HW6/>
+
+        </div>
+    )
+}
+
+export default Junior
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HW6 from "../../h6/HW6";
+
+function JuniorPlus() {
+    return (
+        <div>
+            <HW6/>
+
+        </div>
+    )
+}
+
+export default JuniorPlus
