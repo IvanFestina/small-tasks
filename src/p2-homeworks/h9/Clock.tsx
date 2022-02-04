1bf33cb9eca265f92041086a7f2864cee59fd8f5
IvanFestina <ivanfestina@gmail.com>
2022-02-04 18:08:33 +0300

    done HW 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {Typography} from "@mui/material";
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
@@ -7,45 +8,45 @@ function Clock() {
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
-        const id: number = window.setInterval(() => {
-            // setDate
+        const id: number = window.setInterval(() => { setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date?.toLocaleTimeString() || <br/>
+    const stringDate = date?.toLocaleDateString() || <br/>
 
     return (
-        <div>
+        <Typography style={{marginTop: '15px'}}>
             <div
+                style={{marginBottom: '15px'}}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
                 {stringTime}
             </div>
 
-            {show && (
-                <div>
+            {show ? (
+                <div style={{marginTop: '15px'}}>
                     {stringDate}
                 </div>
-            )}
+            ) : (<br/>)}
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
 
-        </div>
+        </Typography>
     )
 }
 
